adc62d097f5f488e3dd00c45c3748fd8688b5941
lindingfeng <1758765238@qq.com>
2019-06-25 20:06:55 +0800

    Vue工程化雏形

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    组件1
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
