6a685188ffa5608645bc20befc61790c24894924
lindingfeng <1758765238@qq.com>
2019-12-16 01:49:06 +0800

    Revert "test jenkins5 by PR3"

--- src/projects/mobile/pages/index/index.vue
+++ src/projects/mobile/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index-page">
-    <van-button type="primary" style="width: 100%;" @click="login">成功按钮1111</van-button>
+    <van-button type="primary" style="width: 100%;" @click="login">成功按钮111</van-button>
     <!-- <Swiper /> -->
     <!-- <Amap /> -->
     <!-- <Clipboard /> -->
