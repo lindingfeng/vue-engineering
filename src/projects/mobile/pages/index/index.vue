784d739fd61a009030e79033d4a9c8a409a9d2dc
lindingfeng <1758765238@qq.com>
2020-06-24 15:47:28 +0800

    test_17

--- src/projects/mobile/pages/index/index.vue
+++ src/projects/mobile/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index-page">
-    <van-button type="primary" style="width: 100%;" @click="login">成功按钮0000</van-button>
+    <van-button type="primary" style="width: 100%;" @click="login">成功按钮000</van-button>
     <!-- <Swiper /> -->
     <!-- <Amap /> -->
     <!-- <Clipboard /> -->
